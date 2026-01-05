928f9732a2dfa0c33e9d1687cdb40baa159fb85d
George Mathenge <47426850+georgemathenge@users.noreply.github.com>
2026-01-05 13:45:35 +0300

    Enhance skill item styling with hover effect
    Added hover effect and border to skill items.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,14 +17,14 @@ const Skills = () => {
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
-            <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium text-white">
-                  {skill}
-                </span>
-              </div>
-            </div>
+        <div key={skill} className="p-2 sm:w-1/2 w-full">
+  <div className="bg-gray-800 rounded flex p-4 h-full items-center border border-gray-700 hover:border-green-500 transition duration-300">
+    <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+    <span className="title-font font-medium text-white">
+      {skill}
+    </span>
+  </div>
+</div>
           ))}
         </div>
       </div>
